ca917650bc7f58fe6193b254a6596cf7112dc806
Artasov <ivanhvalevskey@gmail.com>
2025-09-28 19:52:31 +0300

    process

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<img height="85" src="logo.png" width="85"/>
+<img height="85" src="img/logo.png" width="85"/>
 
 <h1 style="margin: 0;">
     <a target="_blank" href="https://artasov.github.io/wide-classes/">DOCS</a>
--- js/device-classifier.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-    let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-        document.body.classList.add('_touch');
-    } else {
-        document.body.classList.add('_pc');
-    }
-});
